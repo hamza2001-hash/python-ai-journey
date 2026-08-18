6b3acd8c36826e8a5e7b409a21a10b5e6e64b2af
Hamza Chaabi <hamzachaabi32@gmail.com>
2026-08-18 13:29:53 -0400

    done

---
+++ Password_Checker.py
@@ -0,0 +1,7 @@
+username = input ("Enter your username: ")
+password = input ("Enter your password: ")
+
+if username == "admin" and password == "python123":
+    print("Login successful!")
+else:
+    print("Login failed!")
