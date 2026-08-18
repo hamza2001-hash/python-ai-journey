5c5ac241046964087b6376d2216662966c5b7818
Hamza Chaabi <hamzachaabi32@gmail.com>
2026-08-18 13:20:42 -0400

    Day 1: Python basics

---
+++ day1_memory.py
@@ -0,0 +1,4 @@
+name = input ("What is your name? ")
+age = int(input("How old are you? "))
+print (f"Hello {name}, you are {age} years old.")
+print(f"Hello {name}, next year you will be {age + 1} years old.")
