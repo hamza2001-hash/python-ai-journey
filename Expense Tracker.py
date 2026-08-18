31044ed62654ef6af517a522570532e22fe7e8a4
Hamza Chaabi <hamzachaabi32@gmail.com>
2026-08-18 16:21:42 -0400

    adding the largest and the minimum expense

--- Expense Tracker.py
+++ Expense Tracker.py
@@ -1,7 +1,10 @@
-expense = 0
+expenses = []
 
 for i in range(5):
-    expense += float(input(f"Enter expense {i + 1}: "))
+    expense = float(input(f"Enter expense {i + 1}: "))
+    expenses.append(expense)
 
-print(f"Total expenses: ${expense:.2f}")
-print(f"Average expense: ${expense / 5:.2f}")
+print(f"Total expenses: ${sum(expenses):.2f}")
+print(f"Average expense: ${sum(expenses) / 5:.2f}")
+print(f"Highest expense: ${max(expenses):.2f}")
+print(f"Lowest expense: ${min(expenses):.2f}")
